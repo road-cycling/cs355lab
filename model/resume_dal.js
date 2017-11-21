fadb57708f4b17907b2a4cfde803e4e323710ec7
nieroda <eathotlead@gmail.com>
2017-11-20 23:14:30 -0800

    progress made

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -11,3 +11,30 @@ exports.getAll = () => {
     });
   });
 }
+
+exports.schoolName = (id) => {
+  return new Promise((resolve, reject) => {
+    let myquery = `select school_name from account_school left join school on school.school_id = account_school.school_id where account_id=${connection.escape(id)};`;
+    connection.query(myquery, (err, result) => {
+      err ? reject(err) : resolve(result);
+    });
+  });
+}
+
+exports.companyName = (id) => {
+  return new Promise((resolve, reject) => {
+    let myquery = `select company_name from account_company left join company_address on company_address.company_id = account_company.company_id left join company on company.company_id = company_address.company_id where account_id=${connection.escape(id)};`;
+    connection.query(myquery, (err, result) => {
+      err ? reject(err) : resolve(result);
+    });
+  });
+}
+
+exports.skillName = (id) => {
+  return new Promise((resolve, reject) => {
+    let myquery = `select skill_name from account_skill left join skill on skill.skill_id = account_skill.skill_id where account_id=${connection.escape(id)};`;
+    connection.query(myquery, (err, result) => {
+      err ? reject(err) : resolve(result);
+    });
+  });
+}
--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var resume_dal = require('../model/resume_dal');
+var insert_dal = require('../model/inserts_universal');
 
 router.get('/add/selectuser', (req, res) => {
   resume_dal.getAll()
@@ -8,9 +9,45 @@ router.get('/add/selectuser', (req, res) => {
   .catch(error => res.send(err));
 });
 
-router.get('/resume/change/:id', (req, res) => {
+router.get('/change/:id', (req, res) => {
   let id = req.params.id;
-
+  Promise.all([resume_dal.schoolName(id), resume_dal.companyName(id), resume_dal.skillName(id)])
+  .then(result => {
+    let [ schools, companies, skills ] = result;
+    res.render('resume_form', {schools, companies, skills, id});
+  })
 });
 
+router.post('/change', (req, res) => {
+  console.log('made request');
+  console.log(req.body);
+  let reqbody = req.body;
+  var resumeinsert = { //there must be a better way
+    account_id: reqbody.account_id,
+    resume_name: reqbody.resume_name
+  }
+  insert_dal.insert('resume', resumeinsert)
+  .then(resp => console.log(resp))
+  .catch(err => console.log(err));
+
+  res.redirect('/resume/add/selectuser');
+});
+//item , data
+
+
 module.exports = router;
+
+
+/*
+es6 is fantastic
+schools.map(item => console.log(item));
+companies.map(item => console.log(item));
+skills.map(item => console.log(item));
+
+called
+RowDataPacket { school_name: 'MIT' }
+RowDataPacket { company_name: 'Google' }
+RowDataPacket { skill_name: 'English' }
+
+
+*/
